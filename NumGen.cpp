5165f9e342d9f1f0c3945902c22973c002c57e9e
davidlao <61684228+davidlao27@users.noreply.github.com>
2022-05-12 17:45:18 +0200

    2022 NumGen 2.0!
    - Changed license to open-source
    - Modified code for 2.0 <3
    - Removed Linux 32-bit support, Windows 32-bit just works, but Linux 32-bit does not want to compile due to a "missing header file".
    - Added file to compile (made for Ubuntu, if not, make sure to have g++ and mingw32-g++ both x86_64 and i686-w64)
    - Modified readme file drastically.
    
    And also don't forget:
    - Snap store updates for NumGen have stopped, at least until further notice :(
    - The new compilation file has -Os optimization, but it's optional!
    - The readme compilation commands have been improved for readability and removed mentioning static-libstd/libgcc for Linux builds (they were useless)
    
    Thank you for being there!
    I'll be pushing compiled release binaries after this.

--- NumGen.cpp
+++ NumGen.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-int nums; int cnum; int seed;int cnum2; int main() {
-cnum = 1; cnum2 = 1;std::cout << "Input a seed number" << std::endl;
-std::cin >> seed;std::cout << "Input the amount of numbers to generate" << std::endl;std::cin >> nums;
-std::cout << "Your filename will be like this: generatednums & numbers*seed" << std::endl;std::cin.get();
-std::cout << "To stop the program you'll have to forcedly stop it with CTRL+C." << std::endl;std::cin.get();
-std::cout << "Seed set to " << seed << std::endl;
-std::cout << "Generating " << nums << " numbers with seed " << seed << "..." << std::endl;
-std::string fname;int calcsu = nums * seed + rand() % 1;std::string calcsust = std::to_string(calcsu);
-fname.append("generatednums");fname.append(calcsust);fname.append(".txt");std::ofstream MyFile(fname);
-MyFile << "GENERATED NUMBERS:" << std::endl;
-while(cnum < nums+1){std::cout << cnum * seed << " num: " << cnum << std::endl;cnum++;}
-std::cout << "Writing to file..." << std::endl;
+using namespace std;
+
+int main() {
+ios_base::sync_with_stdio(false); string fname;
+int nums;int cnum=1;int seed;int cnum2=1;
+cout << "NumGen v2.0\nInput a seed number" << endl;
+
+cin >> seed;cout << "Input the amount of numbers to generate" << endl;cin >> nums;
+cout << "Your filename will be like this: generatednums & numbers*seed" << endl;cin.get();
+cout << "To stop the program you'll have to forcedly stop it with CTRL+C." << endl;cin.get();
+cout << "Seed set to " << seed << endl;
+cout << "Generating " << nums << " numbers with seed " << seed << "..." << endl;
+int calcsu = nums * seed + rand() % 1;string calcsust = to_string(calcsu);
+fname.append("generatednums");fname.append(calcsust);fname.append(".txt");ofstream MyFile(fname);
+MyFile << "GENERATED NUMBERS:" << endl;
+while(cnum < nums+1){cout << cnum * seed << " num: " << cnum << endl;cnum++;}
+cout << "Writing to file..." << endl;
 while(cnum2 < nums+1){
-MyFile << cnum2 * seed;MyFile << " num: ";MyFile << cnum2 << std::endl;cnum2++;}
-std::cout << "OK! Closing file..." << std::endl;
-MyFile.close();std::cout << "SUCCESS." << std::endl;
-std::cout << "Done! Press any key to restart." << std::endl;std::cin.get();std::cout << "\033[2J\033[1;1H";
-std::cout << "Clearing data..." << std::endl;cnum=1;cnum2=1;
-std::cout << "Success!" << std::endl;std::cout << "\033[2J\033[1;1H";main();}
+MyFile << cnum2 * seed;MyFile << " num: ";MyFile << cnum2 << endl;cnum2++;}
+nums = int(); cnum = int(); seed = int(); cnum2 = int(); calcsu = int(); MyFile.close();
+cout << "OK! Press any key to restart." << endl;cin.get();cout << "\033[2J\033[1;1H"; cnum=1;cnum2=1;
+cout << "Success!" << endl;cout << "\033[2J\033[1;1H";main();}
--- README.md
+++ README.md
@@ -1,23 +1,25 @@
 # NumGen
 An integer generator made in C++
 
-### NOW ON THE SNAP STORE!
-[![Get it from the Snap Store](https://snapcraft.io/static/images/badges/en/snap-store-black.svg)](https://snapcraft.io/numgen)
-
 ### Information
-Compiled (on Ubuntu 20.04.2 LTS 64-Bit) with:<br>
-Windows x64: x86_64-w64-mingw32-g++ ...<br>
-Windows x86: i686-w64-mingw32-g++ ...<br>
-Linux x64: g++ ...<br>
-Linux x86: g++ ... -m32<br><br>
+Compiled (on Ubuntu 20.04.2 LTS 64-Bit) with -Os optimization to reduce filesize and maybe increase speed :D<br>
 
-Linked libraries:<br>
+Commands used:<br>
+Windows x64: x86_64-w64-mingw32-g++ NumGen.cpp -static-libstdc++ -static-libgcc -o NumGen_Windows64 -Os<br>
+Windows x86: i686-w64-mingw32-g++ NumGen.cpp -static-libstdc++ -static-libgcc -o NumGen_Windows32 -Os<br>
+Linux x64: g++ NumGen.cpp -o NumGen_Linux64 -Os<br>
+Linux x86: g++ NumGen.cpp -o NumGen_Linux32 -Os -m32<br><br>
+
+Linked (Windows) libraries:<br>
 [-static-libstdc++ -static-libgcc]
 
 ## Features (default, cannot be changed right now)
 - Save generated numbers on a file
 
 ## Also...
-I make Linux and Windows builds. I compile for both, but I mainly look into updating the Linux x86 and x64 ones. If there's an issue on Windows binary builds, please tell me ASAP 
+I make Linux and Windows builds, but I usually only test on Linux and for Windows just the surface.<br>
+If there's an issue on Windows binary builds, please tell me. :D<br><br>
+COMPILATION FOR LINUX 32-BIT DROPPED SINCE 2.0
 
-<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licencia de Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />This work is under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0) license.</a>
+### OUTDATED SNAP VERSION
+[![Get it from the Snap Store](https://snapcraft.io/static/images/badges/en/snap-store-black.svg)](https://snapcraft.io/numgen)
